7b6adfded9a89d65bfc35ea43ddd7c92d83d63ae
outwebfeng <amylindsey958@gmail.com>
2025-01-06 11:37:59 +0800

    添加多语言

--- i18n.ts
+++ i18n.ts
@@ -8,34 +8,34 @@ export const languages = [
     lang: 'en',
     label: 'English',
   },
-  // {
-  //   code: 'es-ES',
-  //   lang: 'es',
-  //   label: 'Español',
-  // },
-  // {
-  //   code: 'fr-FR',
-  //   lang: 'fr',
-  //   label: 'Français',
-  // },
+  {
+    code: 'es-ES',
+    lang: 'es',
+    label: 'Español',
+  },
+  {
+    code: 'fr-FR',
+    lang: 'fr',
+    label: 'Français',
+  },
   {
     code: 'ja-JP',
     lang: 'jp',
     label: '日本語',
   },
-  // {
-  //   code: 'zh-TW',
-  //   lang: 'tw',
-  //   label: '繁體中文',
-  // },
-  // {
-  //   code: 'ko-KR',
-  //   lang: 'ko',
-  //   label: '한국어',
-  // },
+  {
+    code: 'zh-TW',
+    lang: 'tw',
+    label: '繁體中文',
+  },
+  {
+    code: 'ko-KR',
+    lang: 'ko',
+    label: '한국어',
+  },
 ];
 
-export const locales = ['en', 'jp'];
+export const locales = ['en', 'es', 'fr', 'jp', 'tw', 'ko'];
 
 export default getRequestConfig(async ({ locale }) => {
   if (!locales.includes(locale as any)) notFound();
